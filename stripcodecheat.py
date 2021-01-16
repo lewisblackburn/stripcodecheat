35df0b388966a07e78a2d70ea1c608610d1c3a2e
yarnlock <lewisblackburn10@gmail.com>
2021-01-16 11:28:54 +0000

    made api request instead@

--- stripcodecheat.py
+++ stripcodecheat.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from bs4 import BeautifulSoup
+import requests
+from lxml import html
+import json
 
 USERNAME = ""
 PASSWORD = ""
@@ -18,46 +22,39 @@ username.send_keys(USERNAME)
 password.send_keys(PASSWORD)
 submit.send_keys(Keys.RETURN)
 
-time.sleep(5)
+time.sleep(2)
 
 while (True):
-  time.sleep(2)
+  time.sleep(5)
   count = 2 
   for x in range(0, 5):
+    time.sleep(3)
     repo = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[1]/div[' + str(count) + ']/button/span[1]').text
     button = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[1]/div[' + str(count) + ']/button')
     filename = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[2]/h1').text
-    url = "https://github.com/search?q=" + "repo:" + repo + " filename:" + filename
+    url = "https://api.github.com/search/code?q=" + "repo:" + repo + " filename:" + filename
+    response = requests.get(url)
+    data = response.text
+    parsed = json.loads(data)
 
+    try:
+        if (parsed['total_count'] < 1):
+          # not found #
+          count = count + 1
+        else:
+          time.sleep(0.5)
+          button.click()
+          time.sleep(0.5) 
+          nextbutton = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[1]/button')
+          nextbutton.click()
+          break
 
-    driver.execute_script("window.open('" + url + "');")
-    driver.switch_to.window(driver.window_handles[1])
-
-    found = driver.find_element_by_xpath('/html/body/div[4]/main/div/div[3]/div/div[1]/h3').text
-    if (found.startswith("W")):
-       # not found 
-      time.sleep(1)
-      driver.close()
-      driver.switch_to.window(driver.window_handles[0])
-      count = count + 1
-    else:
-      time.sleep(1)
-      driver.close()
-      driver.switch_to.window(driver.window_handles[0])
-      button.click()
-      time.sleep(0.5)
-      nextbutton = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[1]/button')
-      nextbutton.click()
-      break
-
-    if (x == 4):
-      time.sleep(1)
-      button.click()
-      time.sleep(0.5)
-      nextbutton = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[1]/button')
-      nextbutton.click()
-
-
-
-# 6 code results or view all results on GitHub
-# We couldn’t find any code matching 'repo:keon/algorithms filename:build.py'
+        if (x == 4):
+          time.sleep(0.5)
+          button.click()
+          time.sleep(0.5)
+          nextbutton = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[1]/button')
+          nextbutton.click()
+    except:
+      print("rate limit")
+      pass
