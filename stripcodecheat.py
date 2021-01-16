27d3543dd246df6453f896029757f267fd8ae910
yarnlock <lewisblackburn10@gmail.com>
2021-01-16 12:50:47 +0000

    MORE POWER (proxies)

--- stripcodecheat.py
+++ stripcodecheat.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 import requests
 from lxml import html
 import json
+from itertools import cycle
+from fp.fp import FreeProxy
 
 USERNAME = ""
 PASSWORD = ""
@@ -22,23 +24,31 @@ username.send_keys(USERNAME)
 password.send_keys(PASSWORD)
 submit.send_keys(Keys.RETURN)
 
-time.sleep(2)
+time.sleep(1)
 
 while (True):
-  time.sleep(5)
   count = 2 
+  proxy = "https://" + requests.get('http://pubproxy.com/api/proxy?limit=1&format=txt&https=true&type=https').text
+  time.sleep(1)
   for x in range(0, 5):
-    time.sleep(3)
     repo = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[1]/div[' + str(count) + ']/button/span[1]').text
     button = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[1]/div[' + str(count) + ']/button')
     filename = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[2]/h1').text
     url = "https://api.github.com/search/code?q=" + "repo:" + repo + " filename:" + filename
-    response = requests.get(url)
+    response = requests.get(url, proxies={"http": proxy})
     data = response.text
     parsed = json.loads(data)
 
+    print(x, proxy)
+
     try:
-        if (parsed['total_count'] < 1):
+        if (x == 4):
+          time.sleep(0.5)
+          button.click()
+          time.sleep(0.5)
+          nextbutton = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[1]/button')
+          nextbutton.click()
+        elif (parsed['total_count'] < 1):
           # not found #
           count = count + 1
         else:
@@ -48,13 +58,5 @@ while (True):
           nextbutton = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[1]/button')
           nextbutton.click()
           break
-
-        if (x == 4):
-          time.sleep(0.5)
-          button.click()
-          time.sleep(0.5)
-          nextbutton = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div[1]/button')
-          nextbutton.click()
     except:
-      print("rate limit")
       pass
